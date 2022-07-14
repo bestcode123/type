1a648af51efc80dc0ea1d1d7bbd58f10a66da961
bestcode123 <callumfrederiksen97@gmail.com>
2022-07-14 16:20:36 +0100

    starting commit

---
+++ src/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*
+TODO:
+    -> Build TYPE code
+    -> Create a working release
+*/
+int main() {
+    printf("Welcome to the TYPE engine, this project is maintained by bestcode123 on GitHub\n");
+    FILE* fp = fopen("data/test/test.type", "r"); // Load file
+    char oneline[150]; // Here is where all of the application data is loaded by the engine
+    
+    while(!feof(fp)) { // This is where most of the TYPE code is stored
+        
+    }
+
+    return 0;
+}
